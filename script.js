5fe0fc19948f701aab5affd88a7fa6bfc6fe55e4
Inger Bangura <ingerbangura@gmail.com>
2022-07-07 00:39:21 +0100

    javascript started

---
+++ script.js
@@ -0,0 +1,136 @@
+const jobListing = [
+    {
+        id: 1,
+        title: "Photosnap",
+        title: "Senior Frontend Developer",
+        category: "senior-level",
+        img: "images/red-blue-logo.png",
+        img: "images/new-icon.png",
+        button: "featured",
+        desc: `1d ago &nbsp;&nbsp;&nbsp;• &nbsp;&nbsp;&nbsp;Full Time&nbsp;&nbsp;&nbsp;• &nbsp;&nbsp;&nbsp; UK only`
+    },
+
+
+
+    {
+        id: 2,
+        title: "iTone",
+        title: "Fullstack Developer",
+        category: "mid-level",
+        img: "images/company-logo.png",
+        img: "images/new-icon.png",
+        button: "featured",
+        desc: `1d ago &nbsp;&nbsp;&nbsp;• &nbsp;&nbsp;&nbsp;Part Time&nbsp;&nbsp;&nbsp;• &nbsp;&nbsp; &nbsp; Remote only`
+
+    },
+
+
+
+    {
+        id: 3,
+        title: "Manage",
+        title: "Junior Frontend Developer",
+        category: "entry-level",
+        img: "images/new-icon.png",
+        img: "images/company-logo2.png",
+        desc: `5d ago &nbsp;&nbsp;&nbsp;• &nbsp;&nbsp;&nbsp;Contract&nbsp;&nbsp;&nbsp;•
+        &nbsp;&nbsp;&nbsp; UK only`
+
+
+    },
+
+
+    {
+        id: 4,
+        title: "Restful",
+        title: "Software Engineer",
+        category: "mid-level",
+        img: "images/company-logo3.png",
+        desc: `1w ago &nbsp;&nbsp;&nbsp;• &nbsp;&nbsp;&nbsp;Full-time&nbsp;&nbsp;&nbsp;•
+        &nbsp;&nbsp;&nbsp; Worldwide`
+
+    },
+
+
+    {
+        id: 5,
+        title: "Optimal",
+        title: "Junior Backend Developer",
+        category: "entry-level",
+        img: "Images/company-logo4.png",
+        desc: `3w ago &nbsp;&nbsp;&nbsp;• &nbsp;&nbsp;&nbsp;Full-time&nbsp;&nbsp;&nbsp;•
+        &nbsp;&nbsp;&nbsp; UK only`
+    },
+
+
+    {
+        id: 6,
+        title: "Barcelle",
+        title: "Junior Web Developer",
+        category: "entry-level",
+        img: "Images/company-logo5.png",
+        desc: `3w ago &nbsp;&nbsp;&nbsp;• &nbsp;&nbsp;&nbsp;Full-time&nbsp;&nbsp;&nbsp;•
+        &nbsp;&nbsp;&nbsp; UK only`
+
+    },
+
+
+    {
+        id: 7,
+        title: "Afitis",
+        title: "Fullstack Engineer",
+        category: "entry-level",
+        img: "Images/company-logo6.png",
+        desc: `2w ago &nbsp;&nbsp;&nbsp;• &nbsp;&nbsp;&nbsp;Part-time&nbsp;&nbsp;&nbsp;•
+        &nbsp;&nbsp;&nbsp; Europe only`
+    },
+
+
+
+    {
+        id: 8,
+        title: "Tates",
+        title: "Software Engineer",
+        category: "mid-level",
+        img: "Images/company-logo7.png",
+        desc: `4w ago &nbsp;&nbsp;&nbsp;• &nbsp;&nbsp;&nbsp;Full-time&nbsp;&nbsp;&nbsp;•
+        &nbsp;&nbsp;&nbsp; USA only`
+
+    },
+
+
+    {
+        id: 9,
+        title: "Arrow",
+        title: "Backend Developer",
+        category: "senior-level",
+        img: "Images/company-logo8.png",
+        desc: `5w ago &nbsp;&nbsp;&nbsp;• &nbsp;&nbsp;&nbsp;Full-time&nbsp;&nbsp;&nbsp;•
+        &nbsp;&nbsp;&nbsp; UK - Remote only`
+
+    },
+
+
+    {
+        id: 10,
+        title: "Green O",
+        title: "Frontend Developer",
+        category: "mid-level",
+        img: "Images/company-logo9.png",
+        desc: `3w ago &nbsp;&nbsp;&nbsp;• &nbsp;&nbsp;&nbsp;Part-time&nbsp;&nbsp;&nbsp;•
+        &nbsp;&nbsp;&nbsp; Worldwide/remote`
+
+    },
+
+
+];
+
+
+const listingCenter = document.querySelector(".section-container");
+
+
+window.addEventListener("DOMContentLoaded", function () {
+
+    let displayListings = listing - container.map(function (boxes))
+
+});
